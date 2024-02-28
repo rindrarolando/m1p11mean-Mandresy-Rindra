9212b79c2e6879f2c3ca9d7e0c047d734c689a2c
rindrarolando <rindrarolando7@gmail.com>
2024-02-28 18:11:15 +0300

    Appointments crud

--- front/src/app/employee-tasks.service.ts
+++ front/src/app/employee-tasks.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeTasksService {
-  private baseUrl = 'http://localhost:3000'; // Adjust with your actual API URL
-
+  private _apiUrl = environment.apiUrl;
+  
   constructor(private http: HttpClient) { }
 
   getEmployeeDailyTasks(employeeId: string, date: string) {
-    return this.http.get(`${this.baseUrl}/employeeTasks/${employeeId}/daily`, { params: { date } });
+    return this.http.get(`${this._apiUrl}/employeeTasks/${employeeId}/daily`, { params: { date } });
   }
 
   addTask(taskData: any) {
-    return this.http.post(`${this.baseUrl}/employeeTasks`, taskData);
+    return this.http.post(`${this._apiUrl}/employeeTasks`, taskData);
   }
 }
