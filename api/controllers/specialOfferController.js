44f5d5c8c5915067a531609c9bd8f3904a78c0d1
Mandresy Rabenja <mandresyrabenj@gmail.com>
2024-02-28 15:22:42 +0300

    feat: special offer notification

--- api/controllers/specialOfferController.js
+++ api/controllers/specialOfferController.js
@@ -1,15 +1,20 @@
 const helper = require('../helpers/common')
-const employeeService = require('../services/employeeService.js')
-const serviceService = require('../services/serviceService.js')
 const ObjectID = require('mongoose').Types.ObjectId
-const userService = require('../services/userService.js')
+const specialOfferService = require('../services/specialOfferService.js')
 
 const addSpecialOffer = async (req, res) => {
     try {
-        
-        helper.sendResponseMsg(res, 'Préference mis ajouté avec succès', true, 200)
+        // insert special offer into database
+        const reqData = {
+            ...req.body,
+            serviceIds: req.body.serviceIds.map(id => new ObjectID(id))
+        }
+        const specialOffer = await specialOfferService.addSpecialOffer(reqData)
+
+        helper.sendResponseMsg(res, specialOffer, true, 201)
     }
     catch(e){
+        console.log(e);
         helper.prettyLog(`catching ${e}`)
         helper.log2File(e.message,'error')
         return helper.sendResponse(res, 500, {message:e.message, success:false})
