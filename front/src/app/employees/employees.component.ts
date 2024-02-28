a0d33be2dec8787c531036bd56e083ccdc41de89
rindrarolando <rindrarolando7@gmail.com>
2024-02-28 17:14:10 +0300

    Employees crud

---
+++ front/src/app/employees/employees.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-employees',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './employees.component.html',
+  styleUrl: './employees.component.scss'
+})
+export class EmployeesComponent {
+
+}
