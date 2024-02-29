8ce927d925feb49f2bc6e9467013ef8a4a432dfe
Mandresy Rabenja <mandresyrabenj@gmail.com>
2024-02-29 11:41:16 +0300

    feat: add monthly benefit

--- api/controllers/financeController.js
+++ api/controllers/financeController.js
@@ -1,7 +1,7 @@
 const financeService = require('../services/financeService.js')
 const helper = require('../helpers/common')
 
-exports.getMonthlyRevenue = async (req, res) => {
+const getMonthlyRevenue = async (req, res) => {
     try {
         const { year } = req.query
         const monthlyRevenue = await financeService.calculateMonthlyRevenue(year)
@@ -13,7 +13,20 @@ exports.getMonthlyRevenue = async (req, res) => {
     }
 }
 
-exports.getDailyRevenue = async (req, res) => {
+const calculateBenefit = async (req, res) => {
+    const { year, month, expenses } = req.body
+    
+    try {
+        const benefitAmount = await financeService.calculateBenefit(year, month, expenses)
+        return helper.sendResponse(res, {amount: benefitAmount})
+    } catch (err) {
+        helper.prettyLog(`catching ${err}`)
+        helper.log2File(err.message,'error')
+        return helper.sendResponse(res, 500, {message:err.message, success:false})
+    }
+}
+
+const getDailyRevenue = async (req, res) => {
     try {
         const { year, month } = req.query
         const dailyRevenues = await financeService.calculateDailyRevenue(year, month)
@@ -23,4 +36,10 @@ exports.getDailyRevenue = async (req, res) => {
         helper.log2File(err.message,'error')
         return helper.sendResponse(res, 500, {message:err.message, success:false})
     }
+}
+
+module.exports = {
+    calculateBenefit,
+    getDailyRevenue,
+    getMonthlyRevenue
 }
